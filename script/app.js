13155a8e27c392c44d706ade5ac24623fc31d447
Sean Pursani <seanpursani@gmail.com>
2022-01-26 17:06:45 +0000

    added calculate function to be worked on

--- script/app.js
+++ script/app.js
@@ -1,35 +1,55 @@
-calculator = {
-    buttons: document.querySelectorAll("button"), // Calc Buttons
-    input: document.querySelectorAll(".input"), // A Node List
-    eventInterface: document.querySelector(".interface").addEventListener("click", (event) => {
-        const calcButton = event.target;
+const buttons = document.querySelectorAll("button"); 
+const input = document.querySelector("#input"); 
+const interface = document.querySelector(".interface"); 
+let numArr = [];
+let calculateSum = [];
+
+interface.addEventListener("click", (event) => {
+    const calcButton = event.target;
         switch (calcButton.className) {
             case "num": 
                 input.innerHTML += calcButton.innerHTML;
-                num.push(Number(calcButton.innerHTML));
-                console.log(currCalculation);
+                numArr.push(calcButton.innerHTML);
                 break;
             case "op":
-                // joining first number
-                num.join('');
-                // clear the array 
-                // store the operator 
+                // if the most recent value is NaN 
+                calculateSum.push(Number(numArr.join(''))); // joining first number
+                numArr = []; // clear the number array 
+                calculateSum.push(calcButton.innerHTML) // store the operator 
                 input.innerHTML = "";
-                currCalculation.push(calcButton.innerHTML);
-                console.log(currCalculation);
                 break;
             case "res":
-                currCalculation = [];
+                numArr = [];
+                calculateSum = [];
                 input.innerHTML = "";
-                console.log(currCalculation);
             case "equals":
-                // join second number 
-                // do maths 
-                if (currCalculation.length > 0) {
-                }
+                // Can't end on operation HAS to be number UNLESS percent
+                // if anything NaN throw error
+                calculateSum.push(Number(numArr.join(''))); // join second number 
+                console.log(calculateSum);
+                let total = calculate(calculateSum);
+                input.innerHTML = total;
+                numArr = [];
+                calculateSum = [];
+                
         }
     })
+    
+const calculate = (arr) => {
+    let runningTotal = arr[0];
+    for (let i = 1; i< arr.length; i+=2) {
+        if (arr[i] === "*") {
+            runningTotal = runningTotal * arr[i+1];
+        } else if (arr[i] === "%") {
+            runningTotal = runningTotal / 100;
+            // maybe return
+        } else if (arr[i] === "/") {
+            runningTotal = runningTotal / arr[i+1];
+        } else if (arr[i] === "-") {
+            runningTotal = runningTotal - arr[i+1];
+        } else {
+            runningTotal = runningTotal + arr[i+1];
+        }
+    return runningTotal;
+    }
 }
-
-let currCalculation = [];
-let num1 = [];
