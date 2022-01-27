a4befe3c6b53db51761c266254aa93a1e67a957d
Sean Pursani <seanpursani@gmail.com>
2022-01-27 15:08:22 +0000

    added precedence to calculate function by doing */ sum first before +/-

--- script/app.js
+++ script/app.js
@@ -8,6 +8,7 @@ interface.addEventListener("click", (event) => {
     const calcButton = event.target;
         switch (calcButton.className) {
             case "num": 
+                input.innerHTML = "";
                 input.innerHTML += calcButton.innerHTML;
                 numArr.push(calcButton.innerHTML);
                 break;
@@ -26,7 +27,6 @@ interface.addEventListener("click", (event) => {
                 // Can't end on operation HAS to be number UNLESS percent
                 // if anything NaN throw error
                 calculateSum.push(Number(numArr.join(''))); // join second number 
-                console.log(calculateSum);
                 let total = calculate(calculateSum);
                 input.innerHTML = total;
                 numArr = [];
@@ -35,21 +35,39 @@ interface.addEventListener("click", (event) => {
         }
     })
     
-const calculate = (arr) => {
+
+const calculate = (arr) => { 
+
+    // Prevent INFINITY e.g. 6 / = INFINITY instead return 6;
+    if (arr.length === 3 && arr[1] === "/" && arr[2] === 0) {
+        return arr[0];
+    }  
+    
+    // Give mulitply or dive special precedence
+    for (let i = 0; i< arr.length; i++) {
+        if (arr[i] === "*") {
+            let total = 0
+            total = arr[i-1] * arr[i+1];
+            console.log(arr[i-1], arr[i], arr[i+1])
+            arr.splice(i-1, 3, total)
+            console.log(arr);
+        } else if (arr[i] === "/") {
+            let total = 0
+            total = arr[i-1] / arr[i+1];
+            console.log(arr[i-1], arr[i], arr[i+1])
+            arr.splice(i-1, 3, total)
+            console.log(arr);
+        }
+    }
+
     let runningTotal = arr[0];
     for (let i = 1; i< arr.length; i+=2) {
-        if (arr[i] === "*") {
-            runningTotal = runningTotal * arr[i+1];
-        } else if (arr[i] === "%") {
-            runningTotal = runningTotal / 100;
-            // maybe return
-        } else if (arr[i] === "/") {
-            runningTotal = runningTotal / arr[i+1];
+        if (arr[i] === "+") {
+            runningTotal = runningTotal + arr[i+1];
         } else if (arr[i] === "-") {
             runningTotal = runningTotal - arr[i+1];
-        } else {
-            runningTotal = runningTotal + arr[i+1];
         }
-    return runningTotal;
     }
+    return runningTotal;
+    
 }
