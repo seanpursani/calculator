c50b06c26238d4fa965d7014977d09c94fc4334e
Sean Pursani <seanpursani@gmail.com>
2022-01-27 16:01:38 +0000

    added percentage functionality

--- script/app.js
+++ script/app.js
@@ -4,6 +4,7 @@ const interface = document.querySelector(".interface");
 let numArr = [];
 let calculateSum = [];
 
+
 interface.addEventListener("click", (event) => {
     const calcButton = event.target;
         switch (calcButton.className) {
@@ -23,10 +24,9 @@ interface.addEventListener("click", (event) => {
                 calculateSum = [];
                 input.innerHTML = "";
             case "equals":
-                // if anything NaN throw error
                 calculateSum.push(Number(numArr.join(''))); // join second number 
                 let total = calculate(calculateSum);
-                input.innerHTML = total;
+                input.innerHTML = total.toFixed(2);
                 numArr = [];
                 calculateSum = [];
                 
@@ -41,6 +41,11 @@ const calculate = (arr) => {
     if (arr.length === 3 && arr[1] === "/" && arr[2] === 0) {
         return arr[0];
     }  
+
+    // Prevent e.g. 58 * = 0 so instead it = 58;
+    if (arr.length === 3 && arr[1] === "*" && arr[2] === 0) {
+        return arr[0];
+    }  
     
     // Give MULTIPLY and DIVIDE precedence by calculating them first 
     // e.g. [(2, *, 2), +, (2, /, 2), -, (2, *, 2)] ---> [4, +, 1, -, 4]
@@ -48,29 +53,30 @@ const calculate = (arr) => {
         if (arr[i] === "*") {
             let total = 0
             total = arr[i-1] * arr[i+1];
-            console.log(arr[i-1], arr[i], arr[i+1])
             arr.splice(i-1, 3, total)
-            i = 0;
+            i = 0; // reset index to Zero
         } else if (arr[i] === "/") {
             let total = 0
             total = arr[i-1] / arr[i+1];
-            console.log(arr[i-1], arr[i], arr[i+1])
             arr.splice(i-1, 3, total)
-            i = 0;
+            i = 0; // reset index to Zero
+        } else if (arr[i] === "%") {
+            let total = 0
+            total = arr[i-1] / 100;
+            arr.splice(i-1, 3, total)
+            i = 0; // reset index to Zero
         }
     }
 
+    // Do the +/- Calculations
     let runningTotal = arr[0];
     for (let i = 1; i< arr.length; i+=2) {
         if (arr[i] === "+") {
             runningTotal = runningTotal + arr[i+1];
         } else if (arr[i] === "-") {
             runningTotal = runningTotal - arr[i+1];
-        } else if (arr[i] === "%") {
-
-        }
+        } 
     }
+    return runningTotal;
 
-    return runningTotal
-    
 }
